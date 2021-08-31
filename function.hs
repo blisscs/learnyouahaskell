cc4160c2fa25e04e82dd3420807b5afb4dfd4327
Suracheth Chawla <dev1987@gmail.com>
2021-08-31 22:09:22 +0700

    add more functions

--- function.hs
+++ function.hs
@@ -40,8 +40,10 @@ length' [] = 0
 length' (_:xs) = 1 + length' xs
 
 sum' :: (Num a) => [a] -> a
-sum' [] = 0
-sum' (x:xs) = x + sum' xs
+-- sum' [] = 0
+-- sum' (x:xs) = x + sum' xs
+-- sum' xs = foldl (\acc x -> acc + x) 0 xs
+sum' = foldl (+) 0
 
 capital :: String -> String
 capital "" = "Empty string, whoops!"
@@ -51,3 +53,48 @@ describeList :: [a] -> String
 describeList xs = "The list is " ++ case xs of [] -> "empty."
                                                [x] -> "a singleton list."
                                                _ -> "a longer list."
+
+mulThree :: (Num a) => a -> a -> a -> a
+mulThree x y z = x * y * z
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+-- compareWithHundred x = compare 100 x
+compareWithHundred  = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+-- flip' f = g
+--   where g x y = f y x
+flip' f y x = f x y
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n:chain (n `div` 2)
+  | odd n = n:chain (n*3 + 1)
+
+numLongChains :: Int
+-- numLongChains = length (filter isLong (map chain [1..100]))
+--   where isLong xs = length xs > 15
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+-- `elem'` using fold
+-- elem' :: (Eq a) => a -> [a] -> Bool
+-- elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+
+-- map' (a -> b) -> [a] -> [b]
+-- map' f xs = foldr (\x acc -> f x : acc) [] xs
